37d6668d44cce41ab0cf234d435ee1d111bcd962
sandeepmchary <awssandeepchary@gmail.com>
2020-01-07 22:32:51 +0530

    added

--- Udemy/Aws-automation-with-boto3/Session-10-Lambda/start-ec2.py
+++ Udemy/Aws-automation-with-boto3/Session-10-Lambda/start-ec2.py
@@ -1,6 +1,7 @@
 import boto3
 session=boto3.session.Session(profile_name="root")
+#ec2_re=boto3.resource(service_name="ec2",region_name="us-east-2")
 ec2_re=session.resource(service_name="ec2",region_name="us-east-2")
 test_env={"Name":"tag:Env","Values":["Testing"]}
 for each_in in ec2_re.instances.filter(Filters=[test_env]):
-	print (each_in)
+	each_in
