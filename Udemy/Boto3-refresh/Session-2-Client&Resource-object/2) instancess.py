f033d6dd5e9ac70861265b9da50f9bdbe177b517
sandeepmchary <awssandeepchary@gmail.com>
2020-06-10 22:50:17 +0530

    added files

--- Udemy/Boto3-refresh/Session-2-Client&Resource-object/2) instancess.py
+++ Udemy/Boto3-refresh/Session-2-Client&Resource-object/2) instancess.py
@@ -1,6 +1,9 @@
 import boto3
 session=boto3.session.Session(profile_name="root")
 ec2_con_re=session.resource(service_name="ec2",region_name="ap-south-1")
-for each_in in ec2_con_re.instances.all():
+for each_in in ec2_con_re.instances():
     print(each_in.id)
+ins_id=input("Enter the instance id: ")
+for each_ins in ec2_con_re.instances(ins_id):
+    print(each_ins)
     
